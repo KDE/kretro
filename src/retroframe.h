e45ff338c4e3dba37761f84b1d71ef008c07e26b
Seshan Ravikumar <seshan10@me.com>
2025-05-07 13:06:42 -0400

    Render using the same QImage

--- src/retroframe.h
+++ src/retroframe.h
@@ -1,5 +1,4 @@
 #pragma once
-
 #include <QQuickPaintedItem>
 #include <QQuickItem>
 #include <QPainter>
@@ -10,15 +9,16 @@ class RetroFrame : public QQuickPaintedItem
 {
     Q_OBJECT
     QML_ELEMENT
-    Q_PROPERTY(QImage image READ image WRITE setImage NOTIFY imageChanged)
+
 public:
     RetroFrame(QQuickItem *parent = nullptr);
-    Q_INVOKABLE void setImage(const QImage &image);
+    
     void paint(QPainter *painter);
-    QImage image() const;
-Q_SIGNALS:
-    void imageChanged();
-private:
-    QImage m_currentImage;
-};
+    Q_INVOKABLE void updateFrameData(const void *data, unsigned width, unsigned height, size_t pitch, QImage::Format format);
+    
+    void paint(QPainter *painter) override;
 
+private:
+    QImage m_frameBuffer;
+    bool m_needsRgbSwap = false;
+};
