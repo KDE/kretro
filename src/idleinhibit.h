6589376c3fa2de412dbc9d375a63d6e746bb7db1
Seshan Ravikumar <seshan10@me.com>
2025-07-18 22:46:20 -0400

    idleinhibit: add SPDX copyright

--- src/idleinhibit.h
+++ src/idleinhibit.h
@@ -1,3 +1,7 @@
+/*
+    SPDX-License-Identifier: GPL-2.0-or-later
+    SPDX-FileCopyrightText: 2025 Seshan Ravikumar <seshan@sineware.ca>
+*/
 #pragma once
 #include <QObject>
 #include <QQmlEngine>
